f5ed5a8d8db83eca2968d5ad68184bfddd4ce623
kashyapthakkar <47193812+kashyapthakkar@users.noreply.github.com>
2022-02-10 23:16:48 -0500

    New design implemented

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
@@ -20,7 +21,7 @@ use League\CommonMark\Extension\FrontMatter\Data\LibYamlFrontMatterParser;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::with("category")->get()
+        'posts' => Post::latest()->with("category, user")->get()
     ]);
 });
 
@@ -32,6 +33,12 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts->load('category', 'author')
+    ]);
+});
+
+Route::get('/authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts->load('category', 'author')
     ]);
 });
